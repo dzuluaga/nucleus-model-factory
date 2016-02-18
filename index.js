5e0668b03e4bc2514f49c19d26526e5247499521
dzuluaga <dzuluaga@apigee.com>
2016-02-18 15:47:27 -0800

    fixed to consume array of modelSpecs in JSON

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var urljoin = require('url-join');
+var debug = require('debug')('nucleus-model-factory');
 
-function modelFactory( options ) {
-  //console.log( options )
-  var config = options.config;
-  function getHyperLinks( options ) {
+
+function modelFactory(  ) {
+  function getHyperLinks( options, config, sequelize ) {
     return function () {
       var that = this;
       var hyperlinks = [];
@@ -11,37 +11,55 @@ function modelFactory( options ) {
         "rel": ["self"],
         "href": urljoin( config.hateoas.url, config.hateoas.basepath, options.path, this.get('id') )
       };
-      var includeHyperLinks = options.associations.map(function (assoc) {
-        return {
-          "rel": [assoc.rel],
-          "href": urljoin(config.hateoas.url, config.hateoas.basepath, options.path,
-              that.get('id'), '?include=' + encodeURIComponent('[{"name":"' + assoc.rel + '"}]'))
+      var includeHyperLinks = ( options.associations || [] ).map(function (assoc) {
+        var hlinks = [];
+        if( assoc.operator != "$like"  ) {
+          hlinks.push({
+            "rel": [assoc.rel.concat("_include")],
+            "href": urljoin(config.hateoas.url, config.hateoas.basepath, options.path,
+                that.get('id'), '?include=' + encodeURIComponent('[{"name":"' + sequelize.models[assoc.modelName].includeAlias + '"}]'))
+          });
         }
+        if( assoc.type === 'hasMany' ) {
+          hlinks.push({
+            "rel": [assoc.rel.concat('_absolute_path')],
+            "href": urljoin(config.hateoas.url, config.hateoas.basepath, options.path,
+                that.get('id'), sequelize.models[assoc.modelName].path)
+          });
+        }
+        return hlinks;
       });
+
       var customHyperLinks = options.customHyperLinks && options.customHyperLinks.bind( that )() || [];
-      return( hyperlinks.concat( self, includeHyperLinks, customHyperLinks ) );
+      var flat_arr = [].concat.apply([], hyperlinks.concat( self, includeHyperLinks, customHyperLinks ));
+      return flat_arr;
+      //return( hyperlinks.concat( self, includeHyperLinks, customHyperLinks ) );
     }
   }
 
-  function getAssociations(assocs) {
+  function getAssociations( assocs ) {
     return {
       associate: function (models) {
         var that = this;
-        assocs.forEach(function (assoc) {
+        ( assocs || [] ).forEach(function (assoc) {
           that[ assoc.type ]( models[ assoc.modelName ], { foreignKey: assoc.foreignKey, targetKey: assoc.targetKey } )
         })
       }
     }
   };
 
-  function generateModel(options) {
+  function generateModel( _options  ) {
+    var db_to_use = _options.modelConf.dbConnection || 'sequelize';
+    var options = _options.modelConf, sequelize = _options.utils.db_connections[ db_to_use ],
+        config = _options.utils.getConfig(), Sequelize = _options.utils.db_connections.Sequelize;
+    debug( 'generating model for ', options.model,' with database connection ', db_to_use );
     if( !options.includeAlias ) { throw new Error('Set includeAlias attribute from the model.'); };
-    var model = options.sequelize.define(
+    var model = sequelize.define(
         options.model,
         {
           links: {
-            type: new options.DataTypes.VIRTUAL(options.DataTypes.STRING),
-            get: getHyperLinks( options )
+            type: new Sequelize.VIRTUAL( Sequelize.STRING ),
+            get: getHyperLinks( options, config, sequelize )
           }
         },
         {
@@ -58,9 +76,25 @@ function modelFactory( options ) {
     return model;
   };
 
-  return {
-    generateModel: generateModel
+  function generateModelMap( specModels, utils ) {
+    var models = {};
+    specModels.forEach(function( specModel ) {
+      var model = generateModel( { modelConf: specModel, utils: utils } );
+      models[ model.name ] = model;
+    });
+    Object.keys( models ).forEach(function ( modelName ) {
+      if ( models[ modelName ].associate ) {
+        models[ modelName ].associate( models );
+      }
+    });
+    return models;
   }
+
+  module.exports = {
+    generateModel: generateModel,
+    generateModelMap: generateModelMap
+  }
+  return module.exports;
 }
 
-module.exports = modelFactory;
+module.exports = modelFactory();
